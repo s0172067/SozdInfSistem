b6523502292c6bae6d1cc348393cad35a33bf4f0
s0172067 <113241344+s0172067@users.noreply.github.com>
2024-12-18 04:38:39 +0300

    Создание примеров и их вывод в консоль. Сравнение
    7 пункт

--- Panesh1/Program.cs
+++ Panesh1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.VisualBasic;
+using Panesh1;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Panesh1
+{
+    internal static class Program
+    {
+     
+        static void Main()
+        {
+            Client client1 = new Client(1, "Ivan", "Ivanov", "Ivanovich", "89185550633", "ivan@gmail.com", "08.11.96", "2441117777", "good");
+            Client client3 = new Client(2, "Igor", "Igorov", "Igorovich", "89184555623", "igor@gmail.com", "18.05.66", "1111662738", " very good");
+            Client client2 = new Client(3, "Oleg", "Olegov", "Olegovich", "89186655443", "oleg@gmail.com", "03.10.03", "4444220099", "bad");
+
+            Console.WriteLine(client1.toMyString());
+            Console.WriteLine(client1.shortInfo());
+            
+            if (client3.ClientEquals(client1)) { Console.WriteLine("Equal!!!"); } else { Console.WriteLine("No");}
+
+        }
+    }
+}
